49fe111e256e0c7129f765470137103da98ab8af
SkyCong <ssskycong@gmail.com>
2019-05-03 13:02:04 +0800

    V6.2.2 电影url优化

--- src/pages/display/search/search.js
+++ src/pages/display/search/search.js
@@ -52,9 +52,9 @@ export default class Search extends Taro.Component {
 
   async fetchData(){
     let result = await http({      
-      url: 'https://douban.uieee.com/v2/movie/search',
+      // url: 'https://douban.uieee.com/v2/movie/search',
       // url: 'http://t.yushu.im/v2/movie/search', 
-      // url: 'https://www.skycong.xyz/v2/movie/search',
+      url: 'https://www.skycong.xyz/db/v2/movie/search',
       data: {
         count: 12,
         q: this.state.val,
